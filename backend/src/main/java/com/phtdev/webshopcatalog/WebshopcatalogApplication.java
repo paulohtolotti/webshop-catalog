8ff3d0083cd70b22f47bbe265503e22569cd0dd2
Paulo H. <paulohtolotti@alumni.usp.br>
2026-03-22 17:13:21 -0300

    feat: first commit

---
+++ backend/src/main/java/com/phtdev/webshopcatalog/WebshopcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.phtdev.webshopcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebshopcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebshopcatalogApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/phtdev/webshopcatalog/WebshopcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phtdev.webshopcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
