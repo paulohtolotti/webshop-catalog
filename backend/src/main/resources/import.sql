5b5935fa562f7c78b1f5b7e56b462cd230654976
Paulo H. <paulohtolotti@alumni.usp.br>
2026-03-24 21:48:18 -0300

    chore: seed de teste para as entidades produto e tabela de associação

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -4,3 +4,6 @@ INSERT INTO tb_category (name) VALUES  ('Hardware');
 INSERT INTO tb_category (name) VALUES  ('Software');
 
 
+INSERT INTO tb_product (name, description, price, img_url) VALUES ('A storm of swords', 'A song of ice and fire', 65.70, 'https://rrmartin.com/storm');
+
+INSERT INTO tb_category_product (category_id, product_id) VALUES (1, 1);
