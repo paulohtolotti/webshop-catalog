1a31b0530b2af5cca6305a73a25102d54816e9cb
Paulo H. <paulohtolotti@alumni.usp.br>
2026-03-24 21:16:00 -0300

    feat: modelo de domínio inicial de categoria (sem relacionamento)

---
+++ backend/src/main/java/com/phtdev/webshopcatalog/entities/Category.java
@@ -0,0 +1,40 @@
+package com.phtdev.webshopcatalog.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "tb_category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    // private Set<Product> products = new HashSet<>();
+
+    public Category() {
+
+    }
+
+    public Category(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
