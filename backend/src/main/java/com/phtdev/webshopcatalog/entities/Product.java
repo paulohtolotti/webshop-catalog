c6e74d2a0c2873c9f8a752ac50e712395d194e19
Paulo H. <paulohtolotti@alumni.usp.br>
2026-03-24 21:48:37 -0300

    feat: modelagem de domínio para Produto

---
+++ backend/src/main/java/com/phtdev/webshopcatalog/entities/Product.java
@@ -0,0 +1,94 @@
+package com.phtdev.webshopcatalog.entities;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_product")
+public class Product {
+    //    private Double price; IGUAL AO DO CURSO
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(columnDefinition = "TEXT")
+    private String description;
+    @Column(columnDefinition = "DECIMAL(10, 2)")
+    private BigDecimal price;
+    private String imgUrl;
+
+    @ManyToMany(mappedBy = "products")
+    private Set<Category> categories = new HashSet<>();
+
+    public Product() {
+
+    }
+
+    public Product(Long id, String name, String description, BigDecimal price, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.imgUrl = imgUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public void addCategory(Category category) {
+        categories.add(category);
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (!(o instanceof Product product)) return false;
+
+        return id.equals(product.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id.hashCode();
+    }
+}
