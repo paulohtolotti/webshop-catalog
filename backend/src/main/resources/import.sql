077ffdd0ee5a3ce93ac005ae991b7552206a4a01
Paulo H. <paulohtolotti@alumni.usp.br>
2026-03-24 21:15:02 -0300

    chore: configurações do banco H2 para testes locais

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO tb_category (name) VALUES ('Books');
+INSERT INTO tb_category (name) VALUES  ('Comics');
+INSERT INTO tb_category (name) VALUES  ('Hardware');
+INSERT INTO tb_category (name) VALUES  ('Software');
+
+
