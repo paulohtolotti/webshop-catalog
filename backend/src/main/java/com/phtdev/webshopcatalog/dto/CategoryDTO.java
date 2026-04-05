92e681dcf27c87536a3ec69d4589137042383927
Paulo H. <paulohtolotti@alumni.usp.br>
2026-04-05 16:24:57 -0300

    chore: validações no DTO

--- backend/src/main/java/com/phtdev/webshopcatalog/dto/CategoryDTO.java
+++ backend/src/main/java/com/phtdev/webshopcatalog/dto/CategoryDTO.java
@@ -1,6 +1,13 @@
 package com.phtdev.webshopcatalog.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 import java.io.Serializable;
 
-public record CategoryDTO(Long id, String name) implements Serializable {
+public record CategoryDTO(
+        Long id,
+        @NotBlank(message = "Category name can't be empty")
+        @Size(min=2, message = "Category must have at least two characters")
+        String name) implements Serializable {
 }
