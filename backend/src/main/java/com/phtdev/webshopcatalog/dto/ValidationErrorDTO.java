2fb33539bdbaed4cbf70c3afadb297dbc61122bc
Paulo H. <paulohtolotti@alumni.usp.br>
2026-04-05 16:43:10 -0300

    feat: tratamento de exceções para erros do jakarta validatio

--- backend/src/main/java/com/phtdev/webshopcatalog/dto/ValidationErrorDTO.java
+++ backend/src/main/java/com/phtdev/webshopcatalog/dto/ValidationErrorDTO.java
@@ -1,4 +1,22 @@
 package com.phtdev.webshopcatalog.dto;
 
-public class ValidationErrorDTO {
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValidationErrorDTO extends BaseErrorDTO {
+
+    private final List<FieldErrorDTO> fieldErrors = new ArrayList<>();
+
+    public ValidationErrorDTO(String error, int status, String path, Instant timestamp) {
+        super(error, status, path, timestamp);
+    }
+
+    public void addFieldError(String field, String message) {
+        fieldErrors.add(new FieldErrorDTO(field, message));
+    }
+
+    public List<FieldErrorDTO> getFieldErrors() {
+        return fieldErrors;
+    }
 }
--- backend/src/main/java/com/phtdev/webshopcatalog/resources/handlers/ResourceExceptionHandler.java
+++ backend/src/main/java/com/phtdev/webshopcatalog/resources/handlers/ResourceExceptionHandler.java
@@ -1,11 +1,14 @@
 package com.phtdev.webshopcatalog.resources.handlers;
 
 import com.phtdev.webshopcatalog.dto.BaseErrorDTO;
+import com.phtdev.webshopcatalog.dto.FieldErrorDTO;
+import com.phtdev.webshopcatalog.dto.ValidationErrorDTO;
 import com.phtdev.webshopcatalog.service.exceptions.ResourceDuplicatedException;
 import com.phtdev.webshopcatalog.service.exceptions.ResourceNotRegistered;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -16,7 +19,8 @@ import java.time.Instant;
 public class ResourceExceptionHandler {
 
     @ExceptionHandler(ResourceNotRegistered.class)
-    public ResponseEntity<BaseErrorDTO> resourceNotRegisteredException(HttpServletRequest http, ResourceNotRegistered exc) {
+    public ResponseEntity<BaseErrorDTO> resourceNotRegisteredException(HttpServletRequest http,
+                                                                       ResourceNotRegistered exc) {
         HttpStatus status = HttpStatus.NOT_FOUND;
         String path = http.getRequestURI();
         BaseErrorDTO errorDTO = new BaseErrorDTO(exc.getMessage(), status.value(), path, Instant.now());
@@ -25,10 +29,25 @@ public class ResourceExceptionHandler {
     }
 
     @ExceptionHandler(ResourceDuplicatedException.class)
-    public ResponseEntity<BaseErrorDTO> resourceDuplicatedException(HttpServletRequest http, ResourceDuplicatedException exc) {
+    public ResponseEntity<BaseErrorDTO> resourceDuplicatedException(HttpServletRequest http,
+                                                                    ResourceDuplicatedException exc) {
         HttpStatus status = HttpStatus.CONFLICT;
         String path = http.getRequestURI();
         BaseErrorDTO errorDTO = new BaseErrorDTO(exc.getMessage(), status.value(), path, Instant.now());
         return ResponseEntity.status(status).body(errorDTO);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public  ResponseEntity<ValidationErrorDTO> dtoValidationExceptionHandler(HttpServletRequest http,
+                                                                        MethodArgumentNotValidException exc) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_CONTENT;
+
+        ValidationErrorDTO error = new ValidationErrorDTO("Validation error", status.value(), http.getRequestURI(),
+                Instant.now());
+
+        exc.getBindingResult().getFieldErrors().forEach(e -> error.addFieldError(e.getField(),
+                e.getDefaultMessage()));
+
+        return ResponseEntity.status(status).body(error);
     }
 }
