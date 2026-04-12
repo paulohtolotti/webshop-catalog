c2dae891f3e32838b3999838af4845db07af5bcd
Paulo H. <paulohtolotti@alumni.usp.br>
2026-04-12 09:25:39 -0300

    feat: criação do DTO de produtos

--- backend/src/main/java/com/phtdev/webshopcatalog/dto/ProductDTO.java
+++ backend/src/main/java/com/phtdev/webshopcatalog/dto/ProductDTO.java
@@ -1,4 +1,38 @@
 package com.phtdev.webshopcatalog.dto;
 
-public record ProductDTO() {
+import com.phtdev.webshopcatalog.entities.Category;
+import com.phtdev.webshopcatalog.entities.Product;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public record ProductDTO(
+        Long id,
+
+        @NotBlank(message = "Product name can't be empty")
+        @Size(min = 2, message = "Product name must have at least 2 characters")
+        String name,
+
+        @NotBlank(message = "Product description name can't be empty")
+        @Size(min = 5, max = 100, message = "Description must have minimum of 2 characters and a maximum of 100 characters")
+        String description,
+
+        @Positive(message = "Price must be a positive")
+        BigDecimal price,
+
+        String imgUrl,
+
+        @Size(min = 1, message = "Product must have at least one category associated")
+        List<CategoryDTO> categories
+) {
+        public static ProductDTO create(Product entity) {
+                return new ProductDTO(entity.getId(), entity.getName(),
+                        entity.getDescription(), entity.getPrice(),
+                        entity.getImgUrl(),
+                        entity.getCategories().stream().map(CategoryDTO::create).toList()
+                );
+        }
 }
