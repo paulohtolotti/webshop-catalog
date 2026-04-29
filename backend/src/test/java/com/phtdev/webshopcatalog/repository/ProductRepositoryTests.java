228fbf25f4096df33c2dca3e8ba30860545a813d
Paulo H. <paulohtolotti@alumni.usp.br>
2026-04-29 20:28:45 -0300

    chore: removendo import não usados

--- backend/src/test/java/com/phtdev/webshopcatalog/repository/ProductRepositoryTests.java
+++ backend/src/test/java/com/phtdev/webshopcatalog/repository/ProductRepositoryTests.java
@@ -5,8 +5,6 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
-import org.springframework.dao.DataIntegrityViolationException;
-
 import java.util.Optional;
 
 @DataJpaTest
